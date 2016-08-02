2247a33863b15e2789799f2f3625589c31c6643c
quocnamlethe <quocnamlethe@cmail.carleton.ca>
2016-08-02 12:30:25 -0400

    generated eps files

--- CSO_Codes/FarajPlotSOvsSIRdiff.m
+++ CSO_Codes/FarajPlotSOvsSIRdiff.m
@@ -9,28 +9,22 @@ function [] = PlotSOvsSIRdiff()
     x = linspace(0,1);
     
     % Plot CN
-    f = figure;
-    set(f,'name','Switch Off Percentage vs SIR difference','numbertitle','off');
+    
+    
+    PPPSIR = CsoTest.TestBs(1).TestPlot(6).SirData(1,2);
     
     for j = 1:6
         h = [];
-        subplot(2,3,j);
+        f = figure;
+        set(f,'name',['Switch Off Percentage vs SIR difference with Input CD: ', CsoTest.TestBs(1).TestPlot(j).Tag],'numbertitle','off');
+        %subplot(2,3,j);
         hold on;
 
         for k = 1:testNum
-%             SirTemp = zeros(10,3);
-%             for m = 0:9
-%                 SirData = CsoTest.TestBs(k).TestPlot(j).SirData;
-%                 SirIndex = find(SirData(:,1) == 0.1*m);
-%                 avgSirData = mean(SirData(SirIndex,2));
-%                 avgSO = mean(SirData(SirIndex,3));
-%                 SirTemp(m+1,:) = [0.1*m, avgSirData, avgSO];
-%             end
-%             CsoTest.TestBs(k).TestPlot(j).SirData = SirTemp;
         
             plotdata = CsoTest.TestBs(k).TestPlot(j).SirData;
             plotdata(plotdata(1:10,3) < 0,3) = 0;
-            h = [h, plot(plotdata(1:10,3),plotdata(1:10,2),strcat('-',colours(k),markers(k)))];
+            h = [h, plot(plotdata(1:10,3),plotdata(1:10,2) - PPPSIR,strcat('-',colours(k),markers(k)))];
             hold on;
             %plot(x,polyval(p,x),strcat('-',colours(k)));
         end
@@ -38,11 +32,10 @@ function [] = PlotSOvsSIRdiff()
         %axis([-50 50 -50 50]);
         xlabel('Switch Off Percentage');
         ylabel('SIR difference (dB)');
-        title(strcat('Input CD: ', CsoTest.TestBs(1).TestPlot(j).Tag));
+        %title(strcat('Input CD: ', CsoTest.TestBs(1).TestPlot(j).Tag));
+        legend(h,'Random','Genie-aided','Greedy deletion','Greedy construction','Semi-greedy deletion');
         hold off;
     end
-    
-    legend(h,'Random','Genie-aided','Greedy deletion','Greedy construction','Semi-greedy deletion');
-    
+
 end
 
--- CSO_Codes/PlotCDin_vs_CDout.m
+++ CSO_Codes/PlotCDin_vs_CDout.m
@@ -4,6 +4,7 @@ function [] = PlotSOvsCovOut()
     load('data/Test_CDin_vs_CDoutData_1.mat', 'CsoTest');
     
     testNum = 4;
+    markers = ['*' 'o' '+' 'x' 's' 'd' '^'];
     colours = ['k' 'm' 'g' 'r' 'b' 'c' 'y'];
     x = linspace(0,1);
     
@@ -17,11 +18,13 @@ function [] = PlotSOvsCovOut()
         for k = 1:testNum
             plotdata = CsoTest.TestBs(k).TestPlot(m).CdData;
             p = polyfit(plotdata(:,1),plotdata(:,2),1);
-            h = [h, plot(plotdata(:,1),plotdata(:,2),colours(k))];
+            h = [h, plot(plotdata(:,1),plotdata(:,2),strcat('-',colours(k),markers(k)))];
         end
 
         axis([0 1 0 1]);
-        title(['CoV In vs CoV Out for ',CsoTest.TestBs(1).TestPlot(m).Tag,' Switch Off']);
+        xlabel('Input CD');
+        ylabel('Output CD');
+        %title(['CoV In vs CoV Out for ',CsoTest.TestBs(1).TestPlot(m).Tag,' Switch Off']);
         hold off;
 
         legend(h,'Random','Semi-greedy deletion','Greedy deletion','Greedy construction');
