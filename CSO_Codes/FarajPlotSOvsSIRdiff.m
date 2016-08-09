2075690a841598868a391fc1d8d7601432c5e87a
quocnamlethe <quocnamlethe@cmail.carleton.ca>
2016-08-09 10:14:23 -0400

    Fixed SOvsSIR plots

--- CSO_Codes/FarajPlotSOvsSIRdiff.m
+++ CSO_Codes/FarajPlotSOvsSIRdiff.m
@@ -2,77 +2,86 @@ function [] = PlotSOvsSIRdiff()
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 
-    load('data/Test_SOvsSIRdiffFarajData_a3_s6_sir50.mat', 'CsoTest');
+    load('data/Test_SOvsSIRdiffFarajData_a3_s6.mat', 'CsoTest');
+    
+    LegendInfo = {'Random','Genie-aided','Greedy deletion','Greedy construction','Semi-greedy deletion'};
+    LegendPosition = [0.50 0.15 0.45 0.35];
+    
+    FigLocation = 'data\';
     
     algNum = 5;
     testPert = [0 0.129074508 0.278385168 0.450877783 0.728588577 2];
     percentSO = 0:0.05:0.9;
-    
-    ModelParameters = ModelParaSet();
-    ModelParameters.lambda = 200e-6;
-    ModelParameters.metric = 'CD';
-
     markers = ['*' 'o' '+' 'x' 's' 'd' '^'];
     colours = ['k' 'm' 'g' 'r' 'b' 'c' 'y'];
-    x = linspace(0,1);
+    Marker_Size = 8;
+    Line_Width = 2;
+    LabelFontSize = 18;
+    percentile = 50;
     
-    % Plot CN
+    PPPIndex = cat(1,CsoTest.TestBs(1).RawData(:).Perturbation) == 2;
+    PPPSIR = cat(1,CsoTest.TestBs(1).RawData(PPPIndex).SIR);
+    PPPSIR = prctile(PPPSIR,percentile);
     
-    
-    %PPPSIR = CsoTest.TestBs(1).TestPlot(6).SirData(1,2);
-    PPPSIR = mean(cat(1,CsoTest.TestBs(1).RawData(19001:19100).SIR));
-    TotalBs = zeros(19,1);
-    for l = 1:19
-        meanActive = mean(length(cat(1,CsoTest.TestBs(1).RawData(((l-1)*100+1):((l-1)*100+99)).ActiveBs)));
-        meanInactive = mean(length(cat(1,CsoTest.TestBs(1).RawData(((l-1)*100+1):((l-1)*100+99)).InactiveBs)));
+    TotalBs = zeros(length(percentSO),1);
+    for l = 1:length(percentSO)
+        SOIndex = round(cat(1,CsoTest.TestBs(1).RawData.SwitchOff)*1e2) == round(percentSO(l)*1e2);
+        SORaw = CsoTest.TestBs(1).RawData(SOIndex);
+        meanActive = mean(length(cat(1,SORaw.ActiveBs)));
+        meanInactive = mean(length(cat(1,SORaw.InactiveBs)));
         TotalBs(l) = meanActive+meanInactive;
     end
     
-    for j = 1:6
+    for j = 1:length(testPert)
         h = [];
         f = figure;
-
         
-        %subplot(2,3,j);
-        hold on;
-        
-        CDs = zeros(5,1);
+        CDs = zeros(algNum,1);
 
         for k = 1:algNum
             
-            rawIndex = cat(1,CsoTest.TestBs(k).RawData(:).Perturbation) == testPert(j);
+            rawIndex = round(cat(1,CsoTest.TestBs(k).RawData(:).Perturbation)*1e9) == round(testPert(j)*1e9);
             tempRaw = CsoTest.TestBs(k).RawData(rawIndex);
 
             CDs(k) = mean(cat(1,tempRaw(:).CDin));
             
-            meanSIR = zeros(19,2);
-            for l = 1:19
-                tempSIR = mean(cat(1,tempRaw(((l-1)*100+1):((l-1)*100+99)).SIR));
-                meanActive = mean(length(cat(1,tempRaw(((l-1)*100+1):((l-1)*100+99)).ActiveBs)));
-                %meanInactive = mean(length(cat(1,CsoTest.TestBs(k).RawData(((l-1)*100+1):((l-1)*100+99)).InactiveBs)));
+            meanSIR = zeros(length(percentSO),2);
+            for l = 1:length(percentSO)
+                SOIndex = round(cat(1,tempRaw.SwitchOff)*1e2) == round(percentSO(l )*1e2);
+                SORaw = tempRaw(SOIndex);
+                allSIR = cat(1,SORaw(:).SIR);
+                tempSIR = prctile(allSIR,percentile);
+                meanActive = mean(length(cat(1,SORaw(:).ActiveBs)));
                 tempSO = mean((TotalBs(l)-meanActive)/TotalBs(l));
                 meanSIR(l,:) = [tempSO tempSIR];
             end
-%             plotdata = CsoTest.TestBs(k).TestPlot(j).SirData;
-%             plotdata(plotdata(1:10,3) < 0,3) = 0;
-            meanSIR(meanSIR(:,1) < 0,1) = 0;
-            h = [h, plot(meanSIR(:,1),meanSIR(:,2) - PPPSIR,strcat('-',colours(k),markers(k)))];
+            plot(meanSIR(:,1),meanSIR(:,2) - PPPSIR,strcat('-',colours(k),markers(k)),'markersize',Marker_Size,'LineWidth',Line_Width);
             hold on;
-            %plot(x,polyval(p,x),strcat('-',colours(k)));
         end
+        
+        hold off;
         
         meanCD = mean(CDs);
         if meanCD < 0.01
             meanCD = 0;
         end
+        
+        tempTag = num2str(meanCD);
+        tempName = sprintf('Fig_%d_CD_%s%%_SIRDiff_a3_s6_sir95',j,tempTag);
+        FigName=[tempName,'.eps'];
+        
+        set(f,'name',tempName,'numbertitle','off');
+        
         set(f,'name',['Switch Off Percentage vs SIR difference with Input CD: ', num2str(mean(CDs))],'numbertitle','off');
 
         %axis([-50 50 -50 50]);
         xlabel('Switch Off Percentage');
-        ylabel('SIR difference (dB)');
-        %title(strcat('Input CD: ', CsoTest.TestBs(1).TestPlot(j).Tag));
-        legend(h,'Random','Genie-aided','Greedy deletion','Greedy construction','Semi-greedy deletion');
-        hold off;
+        ylabel('$S_{g}(0.95)$ (dB)','Interpreter','latex','FontSize',LabelFontSize);
+        set(gca,'fontsize',LabelFontSize);
+        if j == 1
+            legend(LegendInfo,'Interpreter','latex','Location','Best');
+        end
+        grid on;
     end
 
 end
